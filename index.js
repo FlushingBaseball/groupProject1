abed361db28a94ed4b52cda5b42094899a6376e8
Sam Peck <fullstacksam@protonmail.com>
2023-05-12 13:01:56 -0400

    rolling right along, trying to build the form now

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const MovieDisplayArea = document.querySelector('#Movie-Display-Area')
-
+const MovieDisplayArea = document.querySelector('#Movie-Display-Area');
+const TitleAreaDisplay = document.querySelector('#title-area');
+const movieSearchForm = document.querySelector(`#movieSubmit`);
+//const userSearchQuery = document.querySelector()
 
 
 
 fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_KEY}`)
 .then(resp => resp.json())
 .then( movieList => {
-    //console.log(movieList);
+    console.log(movieList);
+    
+    renderMovie(movieList)
+})
+
+
+
+function renderMovie(movieList){
+    
+    
     const newIMG = document.createElement('img');
     newIMG.src= movieList.Poster;
     MovieDisplayArea.appendChild(newIMG);
- 
+    TitleAreaDisplay.textContent = movieList.Title;
+    
+
+
+}
+
+movieSearchForm.addEventListener('submit', (event) =>{
+
+
+    const FormBTN = document.querySelector('#submitBtn');
+    userMovieText
+
+
+
+
 })
