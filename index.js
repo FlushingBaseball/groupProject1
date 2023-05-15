a5320b3ae67ba6d4166fdb6ac9ce5f2c4a3186e1
Sam Peck <fullstacksam@protonmail.com>
2023-05-15 08:58:41 -0400

    idk

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const MovieDisplayArea = document.querySelector('#Movie-Display-Area');
+
+
+
 const TitleAreaDisplay = document.querySelector('#title-area');
-const movieSearchForm = document.querySelector(`#movieSubmit`);
-//const userSearchQuery = document.querySelector()
+const movieSearchForm = document.querySelector(`#movieSubmitArea`);
+
+const detailPicArea = document.querySelector(`#detailPicture`);
+const plotTextArea = document.querySelector(`#plotTextArea`)
 
 
 
-fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_KEY}`)
+
+
+
+
+
+fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_KEY}`) //Hard coded populate an Image on start 
 .then(resp => resp.json())
 .then( movieList => {
     console.log(movieList);
@@ -14,23 +24,31 @@ fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_KEY}`)
 })
 
 
-
-function renderMovie(movieList){
+//The Function that displays a movie 
+function renderMovie(movieList){    
     
     
-    const newIMG = document.createElement('img');
-    newIMG.src= movieList.Poster;
-    MovieDisplayArea.appendChild(newIMG);
-    TitleAreaDisplay.textContent = movieList.Title;
-    
+    const newIMG = document.createElement('img'); //make a new image element
+    newIMG.src= movieList.Poster; //set the src to the movies new image
+    MovieDisplayArea.appendChild(newIMG) //add the area to the DOM
 
+    newIMG.addEventListener('click', (event) => {
+        console.log('hey I was clicked');
+        detailPicArea.src = movieList.Poster;
+        plotTextArea.textContent = movieList.Plot;
 
+    })
 }
+
+
+
+
+
 
 movieSearchForm.addEventListener('submit', (event) =>{
 
     event.preventDefault();
-    const FormBTN = document.querySelector('#submitBtn');
+    const FormBTN = document.querySelector('#movieSubmitBtn');
     const userSearchQuery = event.target.userMovieText.value;
     
     console.log(userSearchQuery);
@@ -40,13 +58,21 @@ movieSearchForm.addEventListener('submit', (event) =>{
 })
 
 
+//Broke out the Fetch to its own function
 function fetchMovie(userSearchQuery){
     
     fetch(`http://www.omdbapi.com/?t=${userSearchQuery}&apikey=${OMDB_KEY}`)
     .then(resp => resp.json())
     .then( movieList => {
-        console.log(movieList);
-        
+        // console.log(movieList);
+        // console.log(movieList.ok)
         renderMovie(movieList)
-    }) 
+  }) 
 }
+
+
+
+
+
+
+
